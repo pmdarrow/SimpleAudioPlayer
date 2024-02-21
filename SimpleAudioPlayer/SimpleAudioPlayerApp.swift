bbf91ccd8e16b59da4952a4ba7cb0e3cf10a50dd
Peter Darrow <pmdarrow@gmail.com>
2024-02-20 23:42:43 -0400

    Initial Commit

---
+++ SimpleAudioPlayer/SimpleAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAudioPlayerApp.swift
+//  SimpleAudioPlayer
+//
+//  Created by Peter Darrow on 2024-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
